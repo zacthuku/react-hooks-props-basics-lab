aec624626de5f45da5334b1401e8b5bdbd4f6210
Zacharia Ndinguri <zacthuku@gmail.com>
2025-04-11 11:38:48 +0300

    props lab

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Links({ github, linkedin }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={github} target="_blank" rel="noopener noreferrer">{github}</a>
+      <br />
+      <a href={linkedin} target="_blank" rel="noopener noreferrer">{linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
